d18b6755c13f0e53a23f5e7ce867ef4a88d87e4f
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-24 13:42:52 +0900

    feat: init

---
+++ metaverse-core/src/main/kotlin/me/cragon/metaverse/plugin/MetaversePlugin.kt
@@ -0,0 +1,7 @@
+package me.cragon.metaverse.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MetaversePlugin: JavaPlugin() {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "metaverse"
+
+val core = "${rootProject.name}-core"
+
+include(core)
+include("metaverse-core")
