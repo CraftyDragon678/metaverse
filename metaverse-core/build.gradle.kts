b65adcdad068ceb1f142d897a3a95823d1ce00f0
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-24 15:33:35 +0900

    chore: build jar tasks

--- metaverse-core/build.gradle.kts
+++ metaverse-core/build.gradle.kts
@@ -1,3 +1,30 @@
 repositories {
     mavenLocal()
 }
+
+tasks {
+    processResources {
+        filesMatching("**/*.yml") {
+            expand(project.properties)
+        }
+    }
+
+    register<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    register<Jar>("paperJar") {
+        archiveVersion.set("")
+        archiveBaseName.set("metaverse")
+        from(sourceSets["main"].output)
+
+        doLast {
+            copy {
+                from(archiveFile)
+                val plugins = File(rootDir, ".debug/plugins/")
+                into(if (File(plugins, archiveFileName.get()).exists()) File(plugins, "update") else plugins)
+            }
+        }
+    }
+}
