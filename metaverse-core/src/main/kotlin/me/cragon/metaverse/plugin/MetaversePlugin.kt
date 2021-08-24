1f499edc639cef96abb631b6f8a6356930f3c654
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-24 15:34:09 +0900

    feat: log enable in onEnable

--- metaverse-core/src/main/kotlin/me/cragon/metaverse/plugin/MetaversePlugin.kt
+++ metaverse-core/src/main/kotlin/me/cragon/metaverse/plugin/MetaversePlugin.kt
@@ -3,5 +3,7 @@ package me.cragon.metaverse.plugin
 import org.bukkit.plugin.java.JavaPlugin
 
 class MetaversePlugin: JavaPlugin() {
-
+    override fun onEnable() {
+        logger.info("Enabled!")
+    }
 }
