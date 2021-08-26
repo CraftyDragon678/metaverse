bf6af1a07a4cb2e48f693b88d0f716192a39d961
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-26 17:26:57 +0900

    refactor: remove unnecessary import

--- metaverse-core/src/main/kotlin/me/cragon/metaverse/Metaverse.kt
+++ metaverse-core/src/main/kotlin/me/cragon/metaverse/Metaverse.kt
@@ -2,7 +2,6 @@ package me.cragon.metaverse
 
 import me.cragon.metaverse.plugin.MetaversePlugin
 import org.bukkit.scheduler.BukkitTask
-import org.jetbrains.annotations.Nullable
 
 class Metaverse(private val plugin: MetaversePlugin) {
     private var task: BukkitTask? = null
