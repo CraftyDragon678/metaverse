d4b3b1b836495290e41199d23156a5b9c62a53a8
CraftyDragon678 <craftydragon678@gmail.com>
2021-08-27 13:47:27 +0900

    fix: fake entity server lazy binding, add all online players

--- metaverse-core/src/main/kotlin/me/cragon/metaverse/Metaverse.kt
+++ metaverse-core/src/main/kotlin/me/cragon/metaverse/Metaverse.kt
@@ -1,10 +1,20 @@
 package me.cragon.metaverse
 
+import io.github.monun.tap.fake.FakeEntityServer
 import me.cragon.metaverse.plugin.MetaversePlugin
+import org.bukkit.Bukkit
 import org.bukkit.scheduler.BukkitTask
 
 class Metaverse(private val plugin: MetaversePlugin) {
     private var task: BukkitTask? = null
+    val fakeEntityServer by lazy {
+        val fes = FakeEntityServer.create(plugin)
+        Bukkit.getOnlinePlayers().forEach {
+            fes.addPlayer(it)
+        }
+        fes
+    }
+
     fun startTask() {
         if (task != null) return
         task = MetaverseTask().runTaskTimer(plugin, 0L, 1L)
