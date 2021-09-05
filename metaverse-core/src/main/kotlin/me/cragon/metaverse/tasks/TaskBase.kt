4735a4f11b72f5c914616f4f85d46dd5ccd1cfaf
CraftyDragon678 <craftydragon678@gmail.com>
2021-09-05 13:41:28 +0900

    feat: task base

---
+++ metaverse-core/src/main/kotlin/me/cragon/metaverse/tasks/TaskBase.kt
@@ -0,0 +1,65 @@
+package me.cragon.metaverse.tasks
+
+import io.github.monun.tap.fake.FakeEntity
+import net.kyori.adventure.bossbar.BossBar
+import net.kyori.adventure.text.Component
+import net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy
+import net.minecraft.network.protocol.game.PacketPlayOutPlayerInfo
+import net.minecraft.server.level.EntityPlayer
+import org.bukkit.Bukkit
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer
+import org.bukkit.scheduler.BukkitRunnable
+
+abstract class TaskBase: BukkitRunnable() {
+    protected open val maxTick = 3 * 60 * 20
+    protected val runAllPlayers = Bukkit.getOnlinePlayers()::forEach
+
+    protected val npcs = mutableListOf<EntityPlayer>()
+    protected val armorStands = mutableListOf<FakeEntity>()
+
+    protected var tick = -1
+
+    override fun run() {
+        tick++
+        if (tick % 20 == 0) {
+            bossBar.name(Component.text("${tick / 20 / 60}:${((tick / 20) % 60).toString().padStart(2, '0')}"))
+            bossBar.progress(tick / maxTick.toFloat())
+        }
+        if (tick == 0) {
+            runAllPlayers {
+                it.showBossBar(bossBar)
+            }
+        }
+        if (tick == maxTick) {
+            cancel()
+        }
+    }
+
+    override fun cancel() {
+        runAllPlayers {
+            val connection = (it as CraftPlayer).handle.b
+            npcs.forEach { npc ->
+                connection.sendPacket(PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.e, npc))
+                connection.sendPacket(PacketPlayOutEntityDestroy(npc.id))
+            }
+        }
+        armorStands.forEach {
+            it.remove()
+        }
+
+        super.cancel()
+    }
+
+    companion object {
+        @JvmStatic
+        private val bossBar = BossBar.bossBar(Component.text("0:00"),  0f, BossBar.Color.GREEN, BossBar.Overlay.NOTCHED_10)
+
+        fun getScene(sceneNumber: Number): Class<out TaskBase>? {
+            return try {
+                Class.forName("${TaskBase::class.java.packageName}.Scene$sceneNumber").asSubclass(TaskBase::class.java)
+            } catch (e: ClassNotFoundException) {
+                null
+            }
+        }
+    }
+}
